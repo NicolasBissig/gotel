6d85be169ca9db22e2f294935a9e56bd9aeffe31
NicolasBissig <nicbissig@gmail.com>
2024-08-20 15:22:53 +0200

    feat(oapi): middleware array

--- goteloapi/middleware.go
+++ goteloapi/middleware.go
@@ -8,11 +8,19 @@ import (
 	"net/http"
 )
 
+// GotelMiddlewares just registers the GotelMiddleware
+var GotelMiddlewares = []strictnethttp.StrictHTTPMiddlewareFunc{
+	GotelMiddleware,
+}
+
+// GotelMiddleware is a middleware that sets the span name to the operation ID provided from the OpenAPI spec
 func GotelMiddleware(f strictnethttp.StrictHTTPHandlerFunc, operationID string) strictnethttp.StrictHTTPHandlerFunc {
 	return func(ctx context.Context, w http.ResponseWriter, r *http.Request, request interface{}) (response interface{}, err error) {
 		span := trace.SpanFromContext(ctx)
 		// set the operation name to the operation ID
 		span.SetName(fmt.Sprintf("%s %s", r.Method, operationID))
+
+		// TODO: check for error and change status + add the error to the span
 		return f(ctx, w, r, request)
 	}
 }
